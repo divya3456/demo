66160fd68abdd478c4a483e6b9a7d59b1c211a9f
divya3456 <140338068+divya3456@users.noreply.github.com>
2024-01-08 12:13:26 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,12 +1,16 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
 st.header("Types of Flowers")
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
-  st.subheader("Flower Image1:")
+  st.subheader("Lotus:")
   st.image("./image1.jpg",caption="Flower1", width="300",use_column_width=True)
   st.write("Flowers  are  Beautiful:")
 with col2:
-  st.subheader("Flower Image2:")
+  st.subheader("Sunflower:")
   st.image("./image2.jpg",caption="Flower2", width="300",use_column_width=True)
-  st.write("Flowers  are  Beautiful:")
+  st.write("Sunflower rotates towards Sun:)
+with col3:
+  st.subheader("Rose:")
+  st.image("./image3.jpg",caption="Flower3",width="300",use_column_width=True)
+  st.write("Roses  are colorful") 
