c8af6f02f7acc303e36a1844c46f46fd42ce3701
divya3456 <140338068+divya3456@users.noreply.github.com>
2024-01-08 12:01:19 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ st.header("Types of Flowers")
 col1,col2=str2.columns(2)
 with col1:
   st.subheader("Flower Image1:")
-  st.image("./image1.jpg",caption=Flower1", width="300",use_column_width=True)
+  st.image("./image1.jpg",caption="Flower1", width="300",use_column_width=True)
   st.write("Flowers  are  Beautiful:")
 with col2:
  st.subheader("Flower Image2")
