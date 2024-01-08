44fc31d072006b36990b8f7e350927fe7e9cffb7
divya3456 <140338068+divya3456@users.noreply.github.com>
2024-01-08 14:03:27 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,6 +6,7 @@ with col1:
   st.subheader("Lotus:")
   st.image("./image1.jpg",caption="Flower1", width="300",use_column_width=True)
   st.write("Flowers  are  Beautiful:")
+  st.markdown("-----")
 with col2:
   st.subheader("Sunflower:")
   st.image("./image2.jpg",caption="Flower2", width="300",use_column_width=True)
