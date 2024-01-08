412d1fe9c7c575d69196836493acf0325954e067
divya3456 <140338068+divya3456@users.noreply.github.com>
2024-01-08 12:02:59 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,5 +8,5 @@ with col1:
   st.write("Flowers  are  Beautiful:")
 with col2:
   st.subheader("Flower Image2")
-  st.image("./image2.jpg",caption="Flower2' ,width="300",use_column_width=True)
+  st.image("./image2.jpg",caption="Flower2', width="300",use_column_width=True)
   st.write("Flowers  are  Beautiful:")
